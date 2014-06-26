86940582c4849c4de52424a117d36b39c251b873
Harry Simon <hasimon@stanford.edu>
2014-06-26 15:41:43 -0700

    Changes Physics experiment. The experiment now elicits free responses for a given phrase. The given phrases are fall under the categories of gradable adjectives (I think gradeable is the right word), physics, and evolutionary. There is also a Design category.

--- experiments/simple-experiment/js/physicsExp.js
+++ experiments/simple-experiment/js/physicsExp.js
@@ -12,13 +12,20 @@ function make_slides(f) {
         {
             name : "freeResponse",
             //Test Phrases
-            present: [{phrase : "a"},
-                      {phrase : "b"},
-                      {phrase : "c"},
-                      {phrase : "d"},
-                      {phrase : "e"},
-                      {phrase : "f"},
-                      {phrase : "g"}
+            present: [{phrase : "The scissors are sharp", type : "gradableAdj"},
+                      {phrase : "The knife is dull", type : "gradableAdj"},
+                      {phrase : "The tea is hot", type : "gradableAdj"},
+                      {phrase : "The coffee is cold", type : "gradableAdj"},
+                      {phrase : "The tea is sweet", type : "gradableAdj"},
+                      {phrase : "The coffee is bitter", type : "gradableAdj"},
+                      {phrase : "The motorcycle is fast", type : "gradeableAdj"},
+                      {phrase : "The car is slow", type : "gradeableAdj"}, 
+                      {phrase : "The apple fell from the tree", type : "physics"},
+                      {phrase : "The ball rolled down the hill", type : "physics"},
+                      {phrase : "The clock ticked", type : "designer"},
+                      {phrase : "The tiger has stripes", type :"evolutionary"},
+                      {phrase : "The bird has wings", type: "evolutionary"},
+                      {phrase : "Fish have scales", type : "evolutionary"}
                      ],
 
             start : function() {
@@ -27,12 +34,15 @@ function make_slides(f) {
             present_handle : function(stim) {
                 console.log(stim);
                 exp.data_trials.push(stim);
+                console.log(exp.data_trials);
 
                 $("#phraseInput").val("");
                 //Get Phrase
-                exp.phys_phrase = stim.phrase;
+                exp.phrase = stim.phrase;
+                exp.phraseType = stim.phraseType;
                 //Update Phrase
-                $('.physPhrase').each(function(){$(this).text(exp.phys_phrase);});
+                $('.physPhrase').each(function(){$(this).text(exp.phrase);});
+
             },
 
             button : function() {
@@ -44,7 +54,7 @@ function make_slides(f) {
                 console.log(res);
                 _.last(exp.data_trials)["phraseInput"] = $("#phraseInput").val();
                 if (_.last(exp.data_trials)["phraseInput"] != "") {
-                     exp.data_trials.push(res);
+                     //exp.data_trials.push(res);
                     _stream.apply(this);
                 }
             }
@@ -52,154 +62,154 @@ function make_slides(f) {
     );
 
 
-    // slides.textInput_ranking = slide(
-    //     {
-    //         name : "textInput_ranking",
-    //         present: [{prop: .6, num_samples:10},
-    //                   {prop: .4, num_samples:10},
-    //                   {prop: .5, num_samples:10},
-    //                   {prop: 1, num_samples:10},
-    //                   {prop: .2, num_samples:10},
-    //                   {prop: 0, num_samples:10},
-    //                   {prop: .8, num_samples:10}
-    //                  ],
+    slides.textInput_ranking = slide(
+        {
+            name : "textInput_ranking",
+            present: [{prop: .6, num_samples:10},
+                      {prop: .4, num_samples:10},
+                      {prop: .5, num_samples:10},
+                      {prop: 1, num_samples:10},
+                      {prop: .2, num_samples:10},
+                      {prop: 0, num_samples:10},
+                      {prop: .8, num_samples:10}
+                     ],
 
-    //         start : function() {
-    //         },
+            start : function() {
+            },
 
-    //         present_handle : function(stim) {
-    //             console.log(stim);
-    //             exp.data_trials.push(stim);
+            present_handle : function(stim) {
+                console.log(stim);
+                exp.data_trials.push(stim);
 
-    //             $("#horse1_rank").val("");
-    //             $("#horse2_rank").val("");
-    //             $("#horse3_rank").val("");
-    //             //get new horse names
-    //             exp.horse_names = [get_horse_name(),get_horse_name(), get_horse_name()];
+                $("#horse1_rank").val("");
+                $("#horse2_rank").val("");
+                $("#horse3_rank").val("");
+                //get new horse names
+                exp.horse_names = [get_horse_name(),get_horse_name(), get_horse_name()];
 
-    //             //update horses names
-    //             $('.horse1_name').each(function(){$(this).text(exp.horse_names[0]);});
-    //             $('.horse2_name').each(function(){$(this).text(exp.horse_names[1]);});
-    //             $('.horse3_name').each(function(){$(this).text(exp.horse_names[2]);});
-
-
-    //             //update table with win data
-    //             $('#horse2').text(Math.round(stim.num_samples * stim.prop) + "");
-    //             $('#horse1').text(Math.round(stim.num_samples * (1-stim.prop)*.85) +"");
-    //             $('#horse3').text(Math.round(stim.num_samples * (1-stim.prop)*.15) +"");
-    //             $('#race_total').text(stim.num_samples);
-    //         },
-
-    //         button : function() {
-    //             var res = {};
-    //             $('input[type="text"]').each( //Makes sure each input is filled
-    //                 function(){
-    //                     res[$(this).attr("name")] = $(this).val();
-    //                 });
-    //             console.log(res);
-    //             _.last(exp.data_trials)["rank1"] =  $("#horse1_rank").val();
-    //             _.last(exp.data_trials)["rank2"] =  $("#horse2_rank").val();
-    //             _.last(exp.data_trials)["rank3"] =  $("#horse3_rank").val();
-
-    //             if (_.last(exp.data_trials)["rank1"] != "" && _.last(exp.data_trials)["rank2"] != "" && _.last(exp.data_trials)["rank3"] != "") {
-    //                  exp.data_trials.push(res);
-    //                 _stream.apply(this);
-    //             }
-    //         }
-    //     }
-    // );
-
-    // slides.repeated_stims = slide(
-    //     {
-    //         name : "repeated_stims",
-    //         present: [{prop: .6, num_samples:5},
-    //                   {prop: .4, num_samples:5},
-    //                   {prop: .5, num_samples:4},
-    //                   {prop: 1, num_samples:5},
-    //                   {prop: .2, num_samples:5},
-    //                   {prop: 0, num_samples:5},
-    //                   {prop: .8, num_samples:5}
-    //                  ],
-
-    //         start: function(){
-    //             // example of moving html blocks around
-    //             // $('#res_sum').prependTo("#baseline");
-    //             // $('#test_sliders').insertAfter("#res_sum");
-    //             // $('<br/><br/><br/>').insertBefore("#test_sliders");
-    //         },
-
-    //         present_handle : function(stim){
-    //             console.log(stim);
-    //             this.init_sliders();
-    //             exp.data_trials.push(stim);
-
-    //             //get new horse names
-    //             exp.horse_names = [get_horse_name(),get_horse_name()];
-
-    //             //update horses names
-    //             $('.horse1_name').each(function(){$(this).text(exp.horse_names[0]);});
-    //             $('.horse2_name').each(function(){$(this).text(exp.horse_names[1]);});
-
-    //             //update table with win data
-    //             $('#horse2').text(Math.round(stim.num_samples * stim.prop) + "");
-    //             $('#horse1').text(Math.round(stim.num_samples * (1-stim.prop)) +"");
-    //             $('#race_total').text(stim.num_samples);
-    //         },
-    //         button : function() {
-    //             if(_.all(exp.sliderPost, function(x){return(x<500);})){
-    //                 _.last(exp.data_trials)["version"]= "baseline";
-    //                 _.last(exp.data_trials)["pid"]= this.phaseid;
-    //                 _.last(exp.data_trials)["prop_estimate"]= exp.sliderPost[0];
-    //                 _.last(exp.data_trials)["confidence"]= exp.sliderPost[1];
-    //                 if(this.version=="pre") exp.data_trials.push($.extend(true, {}, _.last(exp.data_trials)));
-    //                 if(exp.canvas) exp.canvas.remove();
-    //                 _stream.apply(this);
-    //             }
-    //         },
-    //         init_sliders : function() {
-
-    //             exp.sliderPost=[500,500];
-
-    //             if (this.version === "baseline") {
-    //                 $('#test_text').hide();
-    //                 $('#baseline_test_text').show();
-    //             }
-    //             else{
-    //                 $('#test_text').show();
-    //                 $('#baseline_test_text').hide();
-    //             }
-
-    //             $(".sim_slide").css('width' , 3*(exp.width/4)).centerhin();
-    //             $(".slider-lbl1 ").css('right' , (exp.width/4) *3.2 +20);
-    //             $(".slider-lbl2 ").css('left' , (exp.width/4) *3.2 +20);
-    //             $(".slider-lbl3 ").css('left' , (exp.width/2));
-    //             $("#test_slide").slider({
-    //                 range : "min",
-    //                 value : 50,
-    //                 min : 0,
-    //                 max : 100,
-    //                 slide : function(event, ui) {
-    //                     exp.sliderPost[0] = ui.value/100;
-    //                 }
-    //             });
-    //             $("#test_slide").mousedown(function(){$("#test_slide a").css('display', 'inherit');});
-    //             $("#test_slide").slider("option","value",0);//reset slider
+                //update horses names
+                $('.horse1_name').each(function(){$(this).text(exp.horse_names[0]);});
+                $('.horse2_name').each(function(){$(this).text(exp.horse_names[1]);});
+                $('.horse3_name').each(function(){$(this).text(exp.horse_names[2]);});
 
 
-    //             $("#confidence_slide").slider({
-    //                 range : "min",
-    //                 value : 50,
-    //                 min : 0,
-    //                 max : 100,
-    //                 slide : function(event, ui) {
-    //                     exp.sliderPost[1] = ui.value/100;
-    //                 }
-    //             });
-    //             $("#confidence_slide").mousedown(function(){$("#confidence_slide a").css('display', 'inherit');});
-    //             $("#confidence_slide").slider("option","value",0);//reset slider
-    //             $(".ui-slider-handle").css('display', 'none');
-    //         }
-    //     });
+                //update table with win data
+                $('#horse2').text(Math.round(stim.num_samples * stim.prop) + "");
+                $('#horse1').text(Math.round(stim.num_samples * (1-stim.prop)*.85) +"");
+                $('#horse3').text(Math.round(stim.num_samples * (1-stim.prop)*.15) +"");
+                $('#race_total').text(stim.num_samples);
+            },
+
+            button : function() {
+                var res = {};
+                $('input[type="text"]').each( //Makes sure each input is filled
+                    function(){
+                        res[$(this).attr("name")] = $(this).val();
+                    });
+                console.log(res);
+                _.last(exp.data_trials)["rank1"] =  $("#horse1_rank").val();
+                _.last(exp.data_trials)["rank2"] =  $("#horse2_rank").val();
+                _.last(exp.data_trials)["rank3"] =  $("#horse3_rank").val();
+
+                if (_.last(exp.data_trials)["rank1"] != "" && _.last(exp.data_trials)["rank2"] != "" && _.last(exp.data_trials)["rank3"] != "") {
+                     exp.data_trials.push(res);
+                    _stream.apply(this);
+                }
+            }
+        }
+    );
+
+    slides.repeated_stims = slide(
+        {
+            name : "repeated_stims",
+            present: [{prop: .6, num_samples:5},
+                      {prop: .4, num_samples:5},
+                      {prop: .5, num_samples:4},
+                      {prop: 1, num_samples:5},
+                      {prop: .2, num_samples:5},
+                      {prop: 0, num_samples:5},
+                      {prop: .8, num_samples:5}
+                     ],
+
+            start: function(){
+                // example of moving html blocks around
+                // $('#res_sum').prependTo("#baseline");
+                // $('#test_sliders').insertAfter("#res_sum");
+                // $('<br/><br/><br/>').insertBefore("#test_sliders");
+            },
+
+            present_handle : function(stim){
+                console.log(stim);
+                this.init_sliders();
+                exp.data_trials.push(stim);
+
+                //get new horse names
+                exp.horse_names = [get_horse_name(),get_horse_name()];
+
+                //update horses names
+                $('.horse1_name').each(function(){$(this).text(exp.horse_names[0]);});
+                $('.horse2_name').each(function(){$(this).text(exp.horse_names[1]);});
+
+                //update table with win data
+                $('#horse2').text(Math.round(stim.num_samples * stim.prop) + "");
+                $('#horse1').text(Math.round(stim.num_samples * (1-stim.prop)) +"");
+                $('#race_total').text(stim.num_samples);
+            },
+            button : function() {
+                if(_.all(exp.sliderPost, function(x){return(x<500);})){
+                    _.last(exp.data_trials)["version"]= "baseline";
+                    _.last(exp.data_trials)["pid"]= this.phaseid;
+                    _.last(exp.data_trials)["prop_estimate"]= exp.sliderPost[0];
+                    _.last(exp.data_trials)["confidence"]= exp.sliderPost[1];
+                    if(this.version=="pre") exp.data_trials.push($.extend(true, {}, _.last(exp.data_trials)));
+                    if(exp.canvas) exp.canvas.remove();
+                    _stream.apply(this);
+                }
+            },
+            init_sliders : function() {
+
+                exp.sliderPost=[500,500];
+
+                if (this.version === "baseline") {
+                    $('#test_text').hide();
+                    $('#baseline_test_text').show();
+                }
+                else{
+                    $('#test_text').show();
+                    $('#baseline_test_text').hide();
+                }
+
+                $(".sim_slide").css('width' , 3*(exp.width/4)).centerhin();
+                $(".slider-lbl1 ").css('right' , (exp.width/4) *3.2 +20);
+                $(".slider-lbl2 ").css('left' , (exp.width/4) *3.2 +20);
+                $(".slider-lbl3 ").css('left' , (exp.width/2));
+                $("#test_slide").slider({
+                    range : "min",
+                    value : 50,
+                    min : 0,
+                    max : 100,
+                    slide : function(event, ui) {
+                        exp.sliderPost[0] = ui.value/100;
+                    }
+                });
+                $("#test_slide").mousedown(function(){$("#test_slide a").css('display', 'inherit');});
+                $("#test_slide").slider("option","value",0);//reset slider
+
+
+                $("#confidence_slide").slider({
+                    range : "min",
+                    value : 50,
+                    min : 0,
+                    max : 100,
+                    slide : function(event, ui) {
+                        exp.sliderPost[1] = ui.value/100;
+                    }
+                });
+                $("#confidence_slide").mousedown(function(){$("#confidence_slide a").css('display', 'inherit');});
+                $("#confidence_slide").slider("option","value",0);//reset slider
+                $(".ui-slider-handle").css('display', 'none');
+            }
+        });
 
 
 
@@ -289,6 +299,8 @@ function init() {
     exp.go();
 
 };
+
+
 
 function set_condition(){
     exp.condition={dependent:"hl_num",
